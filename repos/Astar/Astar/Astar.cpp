c42a52f1f1be4a06a66c742ce5bcfdb6d7ef8ac3
FrostyForest <748773880@qq.com>
2022-01-14 00:19:17 +0800

    Signed-off-by: FrostyForest <748773880@qq.com> 增加画图功能

--- repos/Astar/Astar/Astar.cpp
+++ repos/Astar/Astar/Astar.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <graphics.h>
+#include <conio.h>
+#include <Windows.h>
 using namespace std;
 
 vector<vector<int>> distance_of_points = {
@@ -30,18 +33,35 @@ vector<vector<int>> distance_of_points = {
 
 
 int main() {
+	initgraph(1000, 1000);	// 创建绘图窗口
+	for (int y = 0; y < 21; y++) {//画出格子
+		for (int x = 0; x < 21; x++) {
+			rectangle(x * 20, y * 20, x * 20 + 20, y * 20 + 20);
+		}
+	}
+	for (int y = 0; y < 21; y++) {//画出障碍物
+		for (int x = 0; x < 21; x++) {
+			if (distance_of_points[y][x] == 999) {
+				setfillcolor(RED);
+				fillrectangle(x * 20, y * 20, x * 20 + 20, y * 20 + 20);
+			}
+		}
+	}
 	int sum = 0;//迭代次数
 	vector<vector<int>> visited(21, vector<int>(21, 0));
 	vector<vector<int>> unvisited(21, vector<int>(21, 1));
 	vector<vector<int>> path(21, vector<int>(21, 0));
 	vector<vector<vector<int>>> sheet1(21, vector<vector<int>>(21, vector<int>(4, INT_MAX)));//到达这个点之前经过那个点的y和x坐标和到起点的距离
-	int x_begin = 8, y_begin = 11;
-	int x_end = 8, y_end = 9;
+	int x_begin = 12, y_begin = 4;//设置起点
+	int x_end = 8, y_end = 11;//设置终点
+	
 	sheet1[y_begin][x_begin][0] = y_begin;
 	sheet1[y_begin][x_begin][1] = x_begin;
 	sheet1[y_begin][x_begin][2] = 0;//2为到起点距离
 	sheet1[y_begin][x_begin][3] = abs(y_end - y_begin) + abs(x_end + x_begin);//3为到终点的曼哈顿距离
 	while (visited[y_end][x_end] != 1) {
+		setfillcolor(WHITE); fillrectangle(x_begin * 20, y_begin * 20, x_begin * 20 + 20, y_begin * 20 + 20);//起点为白色
+		setfillcolor(YELLOW); fillrectangle(x_end * 20, y_end * 20, x_end * 20 + 20, y_end * 20 + 20);//终点为灰色
 		int min_distance = INT_MAX;//表内最小距离
 		int min_y = 0, min_x = 0;//表内距离最小点的坐标
 		for (int y = 0; y < 21; y++) {//表内找到最小未访问点的坐标和距离
@@ -56,6 +76,9 @@ int main() {
 			}
 		}
 		visited[min_y][min_x] = 1;
+		setfillcolor(GREEN);
+		fillrectangle(min_x * 20, min_y * 20, min_x * 20 + 20, min_y * 20 + 20);
+		Sleep(1 * 100);
 		sum++;//迭代次数+1
 		//处理上点
 		if (min_y + 1 <= 20) {
@@ -97,6 +120,11 @@ int main() {
 	int x_r = x_end; int y_r = y_end;
 	while (x_r != x_begin || y_r != y_begin) {//从终点开始反向输出路径坐标
 		cout << x_r << " " << y_r << endl;
+		setfillcolor(WHITE); fillrectangle(x_begin * 20, y_begin * 20, x_begin * 20 + 20, y_begin * 20 + 20);//起点为白色
+		setfillcolor(YELLOW); fillrectangle(x_end * 20, y_end * 20, x_end * 20 + 20, y_end * 20 + 20);//终点为黄色
+		setfillcolor(BLUE);//路径为蓝色
+		fillrectangle(x_r * 20, y_r * 20, x_r * 20 + 20, y_r * 20 + 20);
+		Sleep(5 * 100);
 		path[y_r][x_r] = 1;
 		int temper_x = x_r;
 		x_r = sheet1[y_r][x_r][1];
@@ -110,6 +138,7 @@ int main() {
 		}
 		cout << endl;
 	}
-
+	_getch();
+	closegraph();
 	return sum;//返回距离
 }
--- repos/画图/画图/源.cpp
+++ repos/画图/画图/源.cpp
@@ -2,9 +2,21 @@
 #include <cmath>
 #include <graphics.h>
 #include <conio.h>
+#include <Windows.h>
 int main() {
-	initgraph(640, 480);	// 创建绘图窗口，大小为 640x480 像素
-	ellipse(10, 400, 300, 1);
+	initgraph(1000, 1000);	// 创建绘图窗口，大小为 640x480 像素
+	for (int y = 0; y < 21; y++) {
+		for (int x = 0; x < 21; x++) {
+			rectangle(x * 20, y * 20, x * 20 + 20, y * 20 + 20);
+		}
+	}
+	Sleep(5 * 1000);
+	setfillcolor(GREEN);
+	for (int y = 0; y < 21; y++) {
+		for (int x = 0; x < 21; x++) {
+			fillrectangle(x * 20, y * 20, x * 20 + 20, y * 20 + 20);
+		}
+	}
 	_getch();
 	closegraph();			// 关闭绘图窗口
 	return 0;
